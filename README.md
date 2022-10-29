bff04e0a72dd6b750deb4b8099b9930911c6a91b
ليزبيث هالا <llulah@live.com>
2022-10-29 16:33:22 -0300

    initial setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Adventures in UIKit
+Explorations on UIKit framework.
+## Viewer
+An image viewer app to explore fundamental concepts like UITableView, UIImageView and FileManager.
---
+++ Viewer/Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viewer
+//
+//  Created by ليزبيث هالا on 27/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
